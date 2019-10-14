f64d1489c3d503b4773af49cdd06cc9be7eaec1e
solomonp90 <solomonraulpena@gmail.com>
2019-10-14 18:03:53 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,17 @@ class StudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:id])
+
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    student = Student.create(first_name:params[:student][:first_name],last_name:params[:student][:last_name])
+    # byebug
+    redirect_to student
+  end
+ 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,11 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+  
+  # def to_s
+  #   "#{self.first_name} #{self.last_name}"
+  # end
+
+
+
 end
